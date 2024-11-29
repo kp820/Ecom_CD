b62c94acae5162f91f82e73d5e39cae0a638754b
DivyaJyothiVundavalli <68144559+DivyaJyothiVundavalli@users.noreply.github.com>
2024-11-29 22:22:05 +0530

    Update main.tf

--- CD_EKS/main.tf
+++ CD_EKS/main.tf
@@ -1,4 +1,4 @@
-#Vpc
+# VPC Module
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -9,7 +9,6 @@ module "vpc" {
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
 
-
   enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -17,20 +16,19 @@ module "vpc" {
   tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
   }
+
   public_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
     "kubernetes.io/role/elb"               = 1
-
   }
+
   private_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
     "kubernetes.io/role/private_elb"       = 1
-
   }
 }
 
-#EKS
-
+# EKS Module
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   cluster_name                   = "my-eks-cluster"
@@ -47,16 +45,90 @@ module "eks" {
       instance_types = var.instance_types
     }
   }
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+# Output cluster_name
+output "cluster_name" {
+  value = module.eks.cluster_name
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+# AWS EKS Cluster Data Source
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
 }
+
+# AWS EKS Cluster Auth Data Source
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+
+
+
+# #Vpc
+# module "vpc" {
+#   source = "terraform-aws-modules/vpc/aws"
+
+#   name = "eks_cluster_vpc"
+#   cidr = var.vpc_cidr
+
+#   azs             = data.aws_availability_zones.azs.names
+#   public_subnets  = var.public_subnets
+#   private_subnets = var.private_subnets
+
+
+#   enable_dns_hostnames = true
+#   enable_nat_gateway   = true
+#   single_nat_gateway   = true
+
+#   tags = {
+#     "kubernetes.io/cluster/my-eks-cluster" = "shared"
+#   }
+#   public_subnet_tags = {
+#     "kubernetes.io/cluster/my-eks-cluster" = "shared"
+#     "kubernetes.io/role/elb"               = 1
+
+#   }
+#   private_subnet_tags = {
+#     "kubernetes.io/cluster/my-eks-cluster" = "shared"
+#     "kubernetes.io/role/private_elb"       = 1
+
+#   }
+# }
+
+# #EKS
+
+# module "eks" {
+#   source                         = "terraform-aws-modules/eks/aws"
+#   cluster_name                   = "my-eks-cluster"
+#   cluster_version                = "1.29"
+#   cluster_endpoint_public_access = true
+#   vpc_id                         = module.vpc.vpc_id
+#   subnet_ids                     = module.vpc.private_subnets
+
+#   eks_managed_node_groups = {
+#     nodes = {
+#       min_size       = 1
+#       max_size       = 3
+#       desired_size   = 2
+#       instance_types = var.instance_types
+#     }
+#   }
+#   tags = {
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
+# }
+
+# data "aws_eks_cluster" "cluster" {
+#   name = module.eks.cluster_id
+# }
+
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_id
+# }
